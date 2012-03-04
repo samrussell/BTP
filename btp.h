8258e1c3579eb452365edb350c78a5ceda1991d9
Sam Russell <sam.h.russell@gmail.com>
2012-03-04 22:39:00 +1300

    64-bit problems fixed, more helper code for chunks

--- btp.h
+++ btp.h
@@ -4,21 +4,26 @@
 #define BTP_MSG_SND_CHECKSUM 0x801
 
 typedef struct _BTP_CHUNK_HEADER {
-    unsigned char checklist[4096];
-    unsigned char hash[8];
-    unsigned char data[32768000];
+    uint64_t idx; // index of this chunk
+    uint64_t size; // size of this chunk (should be 32768000 except last chunk)
+    unsigned char checklist[4096]; // bitmap of each 1000-byte packet
+    uint32_t hash; // hash for verification
+    unsigned char data[32768000]; // contains the data
 } BTP_CHUNK_HEADER;
 
 typedef struct _BTP_FILE_HEADER {
-    off_t size; // size of the file 64-bit
+    uint64_t size; // size of the file 64-bit
     FILE* hFile; // handle to the file
-    off_t nChunks; // number of chunks in the file 64-bit
-    off_t ib1; // index of the chunk loaded in the first buffer
+    uint64_t nChunks; // number of chunks in the file 64-bit
+    uint64_t ib1; // index of the chunk loaded in the first buffer
     BTP_CHUNK_HEADER* pb1; // chunk header for the first buffer
-    off_t ib2; // index of the chunk loaded in the second buffer
+    uint64_t ib2; // index of the chunk loaded in the second buffer
     BTP_CHUNK_HEADER* pb2; // chunk header for the second buffer
-    off_t ibc; // index of the chunk loaded in the check buffer
+    uint64_t ibc; // index of the chunk loaded in the check buffer
     BTP_CHUNK_HEADER* pbc; // chunk header for the check buffer
 } BTP_FILE_HEADER;
 
+BTP_CHUNK_HEADER* btp_load_chunk(BTP_FILE_HEADER* pBFH, uint64_t idx);
 BTP_FILE_HEADER* btp_load_file(char* filename);
+
+uint32_t crc32(uint32_t crc, const void *buf, size_t size);
--- btp_file.c
+++ btp_file.c
@@ -1,16 +1,65 @@
 #define __USE_FILE_OFFSET64
+#define _FILE_OFFSET_BITS 64
+#define _LARGEFILE64_SOURCE
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <memory.h>
 #include <errno.h>
 #include "btp.h"
+
+BTP_CHUNK_HEADER* btp_load_chunk(BTP_FILE_HEADER* pBFH, uint64_t idx){
+    // use the index to fine a 32768000 byte chunk
+    // seek to it, then populate a header
+
+    // allocate header, zero out start (but not 32MB of data)
+    BTP_CHUNK_HEADER* pBCH = malloc(sizeof(BTP_CHUNK_HEADER));
+    memset(pBCH, 0, sizeof(uint64_t)+4096+sizeof(uint32_t));
+
+    // set index and load from position
+    pBCH->idx = idx;
+    fseeko64(pBFH->hFile, idx*32768000, SEEK_SET);
+    pBCH->size = fread(pBCH->data, 1, 3276800, pBFH->hFile);
+
+    // generate hash
+    pBCH->hash = crc32(0, pBCH->data, pBCH->size);
+
+    // return chunk
+    return pBCH;
+}
+
+int btp_check_chunk(BTP_CHUNK_HEADER* pBCH){
+    // check parts received
+    // either here or before fill in "size" and "hash"
+    // hash will come from a request
+    // size will be either 32768000 or come from size of file if last chunk
+    // i always feel self-conscious at this stage of a project
+    // like if my code works it's because im lucky and not because it's well designed
+    // this time it will be different though
+    int i;
+    uint32_t crc;
+    for(i=0; i<4096; i++){
+        if(pBCH->checklist[i]!=0xFF){
+            // get packet resent
+            // if at end of chunk, break loop
+        }
+    }
+    // now run checksum
+    crc = crc32(0, pBCH->data, pBCH->size);
+    if(crc != pBCH->hash){
+        // get it to resend in halves
+        // maybe just throw an error for the test
+        printf("chunk CRC failed\n");
+    }
+    return 0;
+}
 
 BTP_FILE_HEADER* btp_load_file(char* filename){
     // this loads a file, indexes it, then returns a handle
     FILE* hFile=NULL;
-    off_t size=0;
+    uint64_t size=0;
     uint32_t bytesperchunk=32768000;
     int res=0;
 
@@ -27,12 +76,8 @@ BTP_FILE_HEADER* btp_load_file(char* filename){
     BTP_FILE_HEADER* pBFH = calloc(1, sizeof(BTP_FILE_HEADER));
     pBFH->size = size;
     pBFH->hFile=hFile;
-    pBFH->nChunks=size/bytesperchunk+1;
+    pBFH->nChunks=size/32768000+1;
     printf("Number of chunks: %lld\n", pBFH->nChunks);
-    printf("  As signed 64 bit decimal: %Ld\n", pBFH->nChunks);
-    printf("  As unsigned 64 bit hex: 0x%016LX\n", pBFH->nChunks);
-    printf("  Low dword in hex: 0x%08X\n", pBFH->nChunks);
-    printf("  High dword in hex: 0x%08X\n", pBFH->nChunks >> 32);
     printf("Total size of chunks: %lld\n", pBFH->nChunks*bytesperchunk);
     return pBFH;
 }
